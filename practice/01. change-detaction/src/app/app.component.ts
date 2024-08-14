e3eb2f218206594ff0636c6c761b349ccf716022
Kobi Hari <kobi2294@yahoo.com>
2024-08-14 18:43:57 +0300

    change detection practive ready

--- practice/01. change-detaction/src/app/app.component.ts
+++ practice/01. change-detaction/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
+  // 1. add change detection strategy here
 })
 export class AppComponent {
-  title = 'change-detaction';
+  counter = 0;
+
+  // 2. inject ChangeDetectorRef here
+
+  constructor() {
+    setInterval(() => {
+      this.counter++;
+      console.log('Counter:', this.counter);
+    }, 1000);
+
+    // 3. Add another interval that calls detectChanges() every 5 seconds
+  }
+
 }
