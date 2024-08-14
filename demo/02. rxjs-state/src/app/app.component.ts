95b2b8bd50146564517ce2f9f71e142fc32c7b4d
Kobi Hari <kobi2294@yahoo.com>
2024-08-14 23:58:40 +0300

    complexities of rxjs

--- demo/02. rxjs-state/src/app/app.component.ts
+++ demo/02. rxjs-state/src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BehaviorSubject, combineLatest, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss', 
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'rxjs-state';
+  readonly a$ = new BehaviorSubject(0);
+  readonly b$ = new BehaviorSubject(0);
+
+  readonly sum$ = combineLatest([this.a$, this.b$]).pipe(
+    map(([a, b]) => a + b)
+  );
+
+  incrementA() {
+    // only increase the value of a if sum$ is less than 10
+    this.a$.next(this.a$.value + 1);
+  }
+
+  incrementB() {
+    this.b$.next(this.b$.value + 1);
+  }
+
+  
+  readonly firstName$ = new BehaviorSubject('John');
+  readonly lastName$ = new BehaviorSubject('Smith');
+
+  readonly fullName$ = combineLatest([this.firstName$, this.lastName$]).pipe(
+    map(([firstName, lastName]) => `${firstName} ${lastName}`), 
+
+    // debouncTime(0) to avoid the temporary wrong full name to be displayed
+  );
+
+  setName(firstName: string, lastName: string) {
+
+    // would cause a temporary wrong full name to be displayed
+
+    this.firstName$.next(firstName);
+    this.lastName$.next(lastName);
+  }
+
+
+  constructor() {
+    this.fullName$.subscribe(console.log);
+  }
+
+
+
 }
