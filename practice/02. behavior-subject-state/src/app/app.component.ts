311a695cb0e85338fc36a1c040620168cee65a45
Kobi Hari <kobi2294@yahoo.com>
2024-08-14 23:24:55 +0300

    try it yourself 2

--- practice/02. behavior-subject-state/src/app/app.component.ts
+++ practice/02. behavior-subject-state/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  // 1. set change detection strategy to OnPush
 })
 export class AppComponent {
-  title = 'behavior-subject-state';
+  readonly counter$ = interval(1000);
+
+  // 2. Remove the counter property and the constructor
+  counter = 0;
+
+  constructor() {
+    this.counter$.subscribe((value) => {
+      this.counter = value;
+    });
+  }
+
+  // 3. In the html, bind directly to the counter$ observable using the async pipe
 }
