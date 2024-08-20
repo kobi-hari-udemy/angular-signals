568133d40471f2a42b6966acbd0008d9a4eafa3d
Kobi Hari <kobi2294@yahoo.com>
2024-08-20 20:44:42 +0300

    Fixed lecture S02L05

--- project/quiz-app/src/app/app.routes.ts
+++ project/quiz-app/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'quizes', pathMatch: 'full' },
+  { path: 'quizes' },
+];
