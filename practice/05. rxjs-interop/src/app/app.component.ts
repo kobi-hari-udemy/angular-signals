44013f5d3dfdba3c6b45a67210f1127ee0edc320
Kobi Hari <kobi2294@yahoo.com>
2024-08-25 18:49:16 +0300

    practice 5 S04L10

---
+++ practice/05. rxjs-interop/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+  // 1. Create an obserbable that emits a value every second
+
+  // 2. Convert the observable to a signal
+
+
+  readonly myName = signal('John Doe');
+  // 3. Create an observable from the "myName" signal
+
+  // 4. Subscribe to the observable and log the value to the console
+
+
+  ngOnInit() {
+    // 5. challange - repeat steps 1 - 4 in this method
+  }
+
+}
