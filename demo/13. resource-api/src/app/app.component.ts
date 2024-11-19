1de3d2af813990954c6c7ee42f68a8db88a83719
Kobi Hari <kobi2294@yahoo.com>
2024-11-20 01:23:53 +0200

    rxResource demonstrated

--- demo/13. resource-api/src/app/app.component.ts
+++ demo/13. resource-api/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, effect, inject, resource, signal } from '@angular/core';
 import { Color } from './models/color.model';
 import { SearchService } from './services/search.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,15 @@ export class AppComponent {
   readonly service = inject(SearchService);
   readonly keyword = signal('');
 
-  readonly results = resource({
+  // readonly results = resource({
+  //   request: () => (this.keyword()), 
+  //   loader: (obj => this.service.search(obj.request, obj.abortSignal))
+  // });
+
+  readonly results = rxResource({
     request: () => (this.keyword()), 
-    loader: (obj => this.service.search(obj.request, obj.abortSignal))
-  });
+    loader: (obj => this.service.rxSearch(obj.request))
+  })
 
   constructor() {
     effect(() => {
