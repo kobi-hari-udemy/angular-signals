2f8f950700559a3cdbfe2947b284c97e68e18b19
Kobi Hari <kobi2294@yahoo.com>
2024-11-20 00:26:42 +0200

    S06L08 - resource api

---
+++ demo/13. resource-api/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component, effect, inject, resource, signal } from '@angular/core';
+import { Color } from './models/color.model';
+import { SearchService } from './services/search.service';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  readonly service = inject(SearchService);
+  readonly keyword = signal('');
+
+  readonly results = resource({
+    request: () => (this.keyword()), 
+    loader: (obj => this.service.search(obj.request, obj.abortSignal))
+  });
+
+  constructor() {
+    effect(() => {
+      console.log('resource status', this.results.status());
+    });
+  }
+
+  resetResource() {
+    this.results.set([
+      { name: 'Red', code: '#FF0000' },
+      { name: 'Green', code: '#00FF00' },
+      { name: 'Blue', code: '#0000FF' }
+    ])
+  }
+}
