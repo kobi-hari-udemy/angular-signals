6c990e6f953fbba7f20b987129d14178a006eaaf
Kobi Hari <kobi2294@yahoo.com>
2025-06-09 18:30:34 +0300

    S07L18 - rxResource

--- demo/15. books-store-with-resources/src/app/services/state-service.ts
+++ demo/15. books-store-with-resources/src/app/services/state-service.ts
@@ -1,6 +1,8 @@
-import { Injectable, linkedSignal, resource, signal } from '@angular/core';
+import { inject, Injectable, linkedSignal, resource, signal } from '@angular/core';
 import { Book } from '../models/book';
-import { httpResource } from '@angular/common/http';
+import { HttpClient, httpResource } from '@angular/common/http';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,7 @@ import { httpResource } from '@angular/common/http';
 export class StateService {
   readonly apiBase = 'http://localhost:3000/api/books';
   readonly wsBase = 'ws://localhost:3000/ws';
+  readonly http = inject(HttpClient);
 
   #keyword = signal<string>('the');
 
@@ -39,6 +42,16 @@ export class StateService {
     }
   })
 
+  #selectedBook = rxResource({
+    params: () => ({id: this.#selectedBookId() }), 
+    stream: (options) => options.params.id
+      ? this.http.get<Book>(`${this.apiBase}/${options.params.id}`)
+      : of(null), 
+    defaultValue: null
+  });
+
+
+
   get keyword() {
     return this.#keyword.asReadonly();
   }
@@ -49,6 +62,10 @@ export class StateService {
 
   get selectedBookId() {
     return this.#selectedBookId.asReadonly();
+  }
+
+  get selectedBook() {
+    return this.#selectedBook.asReadonly();
   }
 
   setKeyword(value: string) {
