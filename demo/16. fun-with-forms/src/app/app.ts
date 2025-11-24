debd3f2df8341a5c10f60b8d516064bc646ee189
Kobi Hari <kobi2294@yahoo.com>
2025-11-24 19:27:02 +0200

    S08L04 - Basic Field Validation

--- demo/16. fun-with-forms/src/app/app.ts
+++ demo/16. fun-with-forms/src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DinnerReview } from './models/dinner-review.model';
-import { Field, form } from '@angular/forms/signals';
+import { email, Field, form, minLength, required } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-root',
@@ -18,5 +18,11 @@ export class App {
     rating: 1,
   });
 
-  readonly reviewForm = form(this.model);
+  readonly reviewForm = form(this.model, path => {
+    required(path.username);
+    required(path.email);
+    email(path.email);
+    minLength(path.description, 5);
+
+  });
 }
