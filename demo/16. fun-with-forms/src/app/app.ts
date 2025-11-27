da944a1e9030c220956f6983b45b2cdc5cdd574f
Kobi Hari <kobi2294@yahoo.com>
2025-11-27 17:39:26 +0200

    S08L14 - Nested Forms

--- demo/16. fun-with-forms/src/app/app.ts
+++ demo/16. fun-with-forms/src/app/app.ts
@@ -1,7 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DinnerReview } from './models/dinner-review.model';
-import { customError, disabled, email, Field, form, hidden, minLength, readonly, required, validate, validateTree } from '@angular/forms/signals';
+import {
+  customError,
+  email,
+  Field,
+  form,
+  minLength,
+  required,
+  validate,
+} from '@angular/forms/signals';
 
 @Component({
   selector: 'app-root',
@@ -13,64 +21,43 @@ export class App {
   readonly model = signal<DinnerReview>({
     username: 'Kobi Hari',
     role: 'user',
-    email: '',
-    description: 'The dinner was great and we loved it so much',
-    rating: 1,
-    recommendation: 'no-opinion',
+    email: 'kobi2294@yahoo.com',
+    description: 'The dinner was very nice, we enjoyed it so much',
+    food: {
+      rating: 1,
+      recommendation: 'no-opinion',
+    },
+    service: {
+      rating: 1, 
+      recommendation: 'no-opinion'
+    }
   });
 
-  readonly reviewForm = form(this.model, path => {
+  readonly reviewForm = form(this.model, (path) => {
     required(path.username, {
       message: 'Username is required',
     });
-    // required(path.email, {
-    //   message: 'Email is required',
-    //   when: (ctx) => ctx.valueOf(path.role) !== 'author'
-    // });
-    // disabled(path.email, ctx => ctx.valueOf(path.role) === 'author');
-    // readonly(path.email, ctx => ctx.valueOf(path.role) === 'author');
-    hidden(path.email, ctx => ctx.valueOf(path.role) === 'author');
+    required(path.email, {
+      message: 'Email is required',
+      when: (ctx) => ctx.valueOf(path.role) !== 'author',
+    });
     email(path.email, {
       message: 'Email is not in the correct format',
     });
     validate(path.description, (ctx) => {
       const value = ctx.value();
-      const threshold = ctx.valueOf(path.role) === 'author'
-        ? 10
-        : 5;
+      const threshold = ctx.valueOf(path.role) === 'author' ? 10 : 5;
 
       // check that there are at least 10 words
       const wordCount = value.trim().split(/\s+/).length;
       if (wordCount < threshold) {
         return customError({
-          kind: 'min-words', 
-          message: `Description needs to be at least ${threshold} words long (currently there are ${wordCount} words)`
-        })
+          kind: 'min-words',
+          message: `Description needs to be at least ${threshold} words long (currently there are ${wordCount} words)`,
+        });
       }
 
       return undefined;
-    }), 
-    validateTree(path, ctx => {
-      const rating = ctx.valueOf(path.rating);
-      const recommendation = ctx.valueOf(path.recommendation);
-
-      if ((rating >= 4) && (recommendation === 'not-recommend')) {
-        return [
-          customError({
-            kind: 'rating-conflict', 
-            message: 'You cannot give rating of 4 and above and not recommend the dinner', 
-            field: ctx.field.rating
-          }), 
-          customError({
-            kind: 'rating-conflict', 
-            message: 'You cannot give rating of 4 and above and not recommend the dinner', 
-            field: ctx.field.recommendation
-          }), 
-        ];
-      }
-
-      return undefined;
-
-    })
+    });
   });
 }
