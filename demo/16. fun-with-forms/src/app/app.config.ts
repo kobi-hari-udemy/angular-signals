7641d32ee67a33c2deba781ef3beacf3eeff2c55
Kobi Hari <kobi2294@yahoo.com>
2026-07-16 15:05:46 +0300

    S08L06a - Setting State CSS Classes Globally

--- demo/16. fun-with-forms/src/app/app.config.ts
+++ demo/16. fun-with-forms/src/app/app.config.ts
@@ -1,7 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideSignalFormsConfig } from '@angular/forms/signals';
+import { NG_STATUS_CLASSES } from '@angular/forms/signals/compat';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideSignalFormsConfig({
+      classes: {
+        'invalid': ctrl => ctrl.state().invalid() && ctrl.state().touched()
+      }
+    })
   ]
 };
