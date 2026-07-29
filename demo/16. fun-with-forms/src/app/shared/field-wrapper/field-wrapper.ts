2a9b4cefbb541e1ebecba2bb01d08eae3545faca
Kobi Hari <kobi2294@yahoo.com>
2026-07-29 19:19:08 +0300

    S09L04 - State Aware Field Wrapper

--- demo/16. fun-with-forms/src/app/shared/field-wrapper/field-wrapper.ts
+++ demo/16. fun-with-forms/src/app/shared/field-wrapper/field-wrapper.ts
@@ -1,4 +1,5 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, contentChild, input } from '@angular/core';
+import { FormField } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-field-wrapper',
@@ -6,7 +7,11 @@ import { Component, input } from '@angular/core';
   templateUrl: './field-wrapper.html',
   styleUrl: './field-wrapper.scss',
 })
-export class FieldWrapper {
+export class FieldWrapper<T> {
   readonly label = input('');
+  readonly fieldDirective = contentChild.required(FormField<T>);
+  readonly fieldState = computed(() => this.fieldDirective().state());
+  readonly errors = computed(() => this.fieldState().errors());
+  readonly required = computed(() => this.fieldState().required());
 
 }
