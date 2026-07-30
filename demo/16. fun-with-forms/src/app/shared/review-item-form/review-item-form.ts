375fcd906ace6ab8e93aa2213023a18c654b2e82
Kobi Hari <kobi2294@yahoo.com>
2026-07-30 18:59:22 +0300

    S09L08 - Custom Controls

--- demo/16. fun-with-forms/src/app/shared/review-item-form/review-item-form.ts
+++ demo/16. fun-with-forms/src/app/shared/review-item-form/review-item-form.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { FieldTree, FormField } from '@angular/forms/signals';
 import { ReviewItem } from '../../models/dinner-review.model';
 import { FieldWrapper } from '../field-wrapper/field-wrapper';
+import { StarRating } from '../star-rating/star-rating';
 
 @Component({
   selector: 'app-review-item-form',
-  imports: [FieldWrapper, FormField],
+  imports: [FieldWrapper, FormField, StarRating],
   templateUrl: './review-item-form.html',
   styleUrl: './review-item-form.scss',
 })
